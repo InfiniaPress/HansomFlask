0004aa63436184ef565ef31929536cac726bfd2e
mintonleeyuimuang <mintonleeyuimuang@gmail.com>
2017-06-13 06:07:28 -0400

    finished DB scripts

--- hansom/Hansom.py
+++ hansom/Hansom.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from hansom.database.db import DBNote, dbsess, getNote
 
 app = Flask(__name__)
 
@@ -10,19 +11,25 @@ class Note:
     self.name = name
     self.contents = contents
     
-note = Note("","")
+#DBnote = DBNote("name","contents", dbsess)
+note = Note("name","contents")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         return render_template("main.html",note=note)
-    note.name = 
-    note.contents = 
+    note.name = #user input
+    note.contents = getNote(note.name)
     return redirect(url_for('index'))
   
 @app.route("/saveNote", methods=["POST"])
 def save():
     note.name = request.json["name"]
     note.contents = request.json["contents"]
-    
+    notes = Note(note.name, note.contents)
+    #notes.
 
+    
+@app.route("/newNote", methods=["POST"])
+def newNote():
+  if 
--- hansom/database/db.py
+++ hansom/database/db.py
@@ -5,17 +5,38 @@ import json
 with open("../../../config.json") as cfg:
     conf = json.load(cfg)
 
-PyMongoDB_URI = "mongodb://{username}:{password}@{hostname}/{databasename}".format(
-    username=conf['db']['username'],
-    password=conf['db']['password'],
-    hostname=conf['db']['hostname'],
-    databasename=conf['db']['dbname'],
-)
-
 
 dbcluster = Cluster([conf['db']['hostname']])
 dbsess = dbcluster.connect(conf['db']['dbname'])
 
 
-class Note():
-  def __init__
+class DBNote():
+  def __init__(self, name, contents, cassandrasess):
+    self.name = name
+    self.contents = contents
+    self.dbsess = cassandrasess
+  def updateDB(self):
+    stmt = dbsess.execute(
+      """
+      BATCH BEGIN;
+      UPDATE hansomdata
+      SET document = %s
+      WHERE username = %s IF EXISTS
+      INSERT INTO hansomdata (username, document)
+      VALUES (%s, %s) IF NOT EXISTS
+      BATCH END;
+      """,
+      (self.name, self.contents)
+    )
+    return stmt
+  
+  
+def getNote(name):
+  stmt = dbsess.execute(
+    """
+    SELECT * FROM hansomdata
+    WHERE username = %s
+    """,
+    (name)
+  )
+  return stmt
