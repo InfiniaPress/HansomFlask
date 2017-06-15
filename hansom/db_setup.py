5b7b30a828b711d9ae9511a068992983ad0cbe45
xiurobert / DerpZ <xiurobert@users.noreply.github.com>
2017-06-15 20:26:18 +0800

    Create db_setup.py

--- hansom/db_setup.py
+++ hansom/db_setup.py
@@ -19,5 +19,28 @@ dbsess = dbcluster.connect(conf['db']['dbname'])
 with open("setup.cql") as cql:
     print("You are about to install the Hansom database. Some text will appear. Do not worry")
     print(cql)
-    dbsess.execute(cql)
+    dbsess.execute(
+        """
+        CREATE KEYSPACE infiniahansom WITH REPLICATION = { 'class' : 'org.apache.cassandra.locator.SimpleStrategy', 'replication_factor': '1' } AND DURABLE_WRITES = true;
+
+CREATE TABLE infiniahansom.hansomdata (
+    username text,
+    document text,
+    PRIMARY KEY (username)
+) WITH read_repair_chance = 0.0
+   AND dclocal_read_repair_chance = 0.1
+   AND gc_grace_seconds = 864000
+   AND bloom_filter_fp_chance = 0.01
+   AND caching = { 'keys' : 'ALL', 'rows_per_partition' : 'NONE' }
+   AND comment = ''
+   AND compaction = { 'class' : 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold' : 32, 'min_threshold' : 4 }
+   AND compression = { 'chunk_length_in_kb' : 64, 'class' : 'org.apache.cassandra.io.compress.LZ4Compressor' }
+   AND default_time_to_live = 0
+   AND speculative_retry = '99PERCENTILE'
+   AND min_index_interval = 128
+   AND max_index_interval = 2048
+   AND crc_check_chance = 1.0;
+
+    """
+    )
     
